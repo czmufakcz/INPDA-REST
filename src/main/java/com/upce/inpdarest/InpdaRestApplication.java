83f7eb2ecf252c9a4f9caca5cb48a281f9c75599
Jakub <jakub.zufanek@gmail.com>
2018-10-02 11:02:28 +0200

    Init project

---
+++ src/main/java/com/upce/inpdarest/InpdaRestApplication.java
@@ -0,0 +1,12 @@
+package com.upce.inpdarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InpdaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InpdaRestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/upce/inpdarest/UsersController.java
@@ -0,0 +1,5 @@
+package com.upce.inpdarest;
+
+public class UsersController {
+
+}
